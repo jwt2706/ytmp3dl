75a7941d0d831a4be3d620ba8f28c05ddcf8f68f
jwt2706 <jwt2home@gmail.com>
2024-05-05 20:41:42 -0400

    Core: support mp3 conversion

--- downloader.py
+++ downloader.py
@@ -1,21 +1,29 @@
-# download audio from youtube only using the link
-# requires the pytube library to work
-
+# for documentation, see README
 
 from pytube import YouTube
 import os
 
+CONVERT = False  # Set to True if you want to convert incoming webm files to mp3
+
+if CONVERT:
+    from pydub import AudioSegment
+
 while True:
     v = input("Video link: ")
     yt = YouTube(v)
-
     video = yt.streams.filter(only_audio=True).first()
-
     out_file = video.download(output_path=".")
 
-    base, ext = os.path.splitext(out_file)
-    new_file = base + '.mp3'
-    os.rename(out_file, new_file)
+    if CONVERT:
+        base, ext = os.path.splitext(out_file)
+        new_file = base + '.mp3'
+        audio = AudioSegment.from_file(out_file, format="webm")
+        audio.export(new_file, format="mp3")
+        os.remove(out_file)
+    else:
+        base, ext = os.path.splitext(out_file)
+        new_file = base + '.webm'
+        os.rename(out_file, new_file)
 
     with open("links.txt", "a") as links:
         links.write(v+"\n")
